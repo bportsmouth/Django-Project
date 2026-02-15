f5ba8fe85ce69bcab16260f8af1b44dfc321460e
Ben Portsmouth <ben.portsmouth205@gmail.com>
2026-02-15 14:10:43 +0000

    Renamed my_page app to monthly_challenges

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mypage.urls'
+ROOT_URLCONF = 'monthly_challenges.urls'
 
 TEMPLATES = [
     {
@@ -66,7 +66,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mypage.wsgi.application'
+WSGI_APPLICATION = 'monthly_challenges.wsgi.application'
 
 
 # Database
